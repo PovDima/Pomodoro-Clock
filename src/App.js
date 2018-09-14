4ab04e6823cde0870bbdbbd7e8ab943c0caabede
PovDima <povdima19@gmail.com>
2018-09-14 16:57:51 +0300

    init commit in separate repository

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Timer from './Timer'
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+      <Timer />
+      </div>
+    );
+  }
+}
+
+export default App;
